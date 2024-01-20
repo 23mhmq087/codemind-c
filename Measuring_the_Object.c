8b9abd8388ebfc2e2a14956dc3214b2ce9c257c4
G.Radha Krishna <147487699+23mhmq087@users.noreply.github.com>
2024-01-20 20:13:03 +0530

    #include<stdio.h> int main() {     int w,x,y,z;     scanf("%d%d%d%d",&w,&x,&y,&z);     int a=x+y;     int b=y+z;     int c=x+z;     if(w==x || w==y ||w==z ||w==a || w==b ||w==c)     printf("YES");     else     printf("NO");
    }

---
+++ Measuring_the_Object.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int w,x,y,z;
+    scanf("%d%d%d%d",&w,&x,&y,&z);
+    int a=x+y;
+    int b=y+z;
+    int c=x+z;
+    if(w==x || w==y ||w==z ||w==a || w==b ||w==c)
+    printf("YES");
+    else
+    printf("NO");
+    
+}
